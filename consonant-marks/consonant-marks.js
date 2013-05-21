cf80a926cb707ca46a03f672f1f77bb157500b6a
Prateek Saxena <prtksxna@gmail.com>
2013-05-21 16:00:38 +0530

    IT WORKS! (Consonant Marks)

--- consonant-marks/consonant-marks.js
+++ consonant-marks/consonant-marks.js
@@ -40,8 +40,68 @@ $(document).ready(function(e){
         };
     });
 
+
+    // GO
     $("#go_tool").bind("click", function(e){
-        alert("Go away");
+        $("#tool_output").html("");
+        // Get form input
+        var c = $("#consonants").val().split("");
+
+        var all_iv = $(".vowels_option");
+        var iv = [];
+        for(var i = 0; i < all_iv.length; i++){
+            if(all_iv[i].checked){
+                iv.push(all_iv[i].value);
+            }
+        }
+
+        var all_vs = $(".various_signs_option");
+        var vs = [];
+        for(var i = 0; i < all_vs.length; i++){
+            if(all_vs[i].checked){
+                vs.push(all_vs[i].value);
+            }
+        }
+
+        var want_reph = $("#need_reph").is(":checked");
+
+
+        // consonants with independent vowels
+        for(var i = 0; i < c.length; i++){
+            var html = c[i] + " ";
+            for(var j = 0; j < iv.length; j++){
+                html += c[i] + dependent_vowels[iv[j]] +" ";
+            }
+            $("#tool_output").append(html);
+
+
+
+            for(var j = 0; j < vs.length; j++){
+                html = " " + c[i] + various_signs[vs[j]] + " ";
+                for(var k = 0; k < iv.length; k++){
+                    html += c[i] + dependent_vowels[iv[k]] + various_signs[vs[j]]  + " ";
+                }
+                $("#tool_output").append(html);
+            }
+
+            if(want_reph){
+                html = reph + c[i] + " ";
+                for(var j = 0; j < iv.length; j++){
+                    html += reph + c[i] + dependent_vowels[iv[j]] + " ";
+                }
+                $("#tool_output").append(html);
+
+                for(var j = 0; j < vs.length; j++){
+                    html = " " + reph + c[i] + various_signs[vs[j]] + " ";
+                    for(var k = 0; k < iv.length; k++){
+                        html += reph + c[i] + dependent_vowels[iv[k]] + various_signs[vs[j]]  + " ";
+                    }
+                    $("#tool_output").append(html);
+                }
+            }
+
+            $("#tool_output").append("<br><br>");
+        }
     });
 
 });
