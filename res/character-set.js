d39c4ed850581a83aa89ff47b03a0234163292d1
Pooja Saxena <anexasajoop@gmail.com>
2013-05-21 13:19:52 +0530

    Created the first revision of the character set of Hindi, Marathi and Sanskrit

---
+++ res/character-set.js
@@ -0,0 +1,107 @@
+var independent_vowels = [
+    "&#x0904", //Letter Short A
+    "&#x0905", //Letter A
+    "&#x0906", //Letter AA
+    "&#x0907", //Letter I
+    "&#x0908", //Letter II
+    "&#x0909", //Letter U
+    "&#x090A", //Letter UU
+    "&#x090B", //Letter Vocalic R
+    "&#x090C", //Letter Vocalic L
+    "&#x090D", //Letter Candra E 
+    "&#x090E", //Letter Short E
+    "&#x090F", //Letter E
+    "&#x0910", //Letter AI
+    "&#x0911", //Letter Candra O
+    "&#x0912", //Letter Short O
+    "&#x0913", //Letter O
+    "&#x0914", //Letter AU
+
+    //Additional vowels for Sanskrit
+    "&#x0960", //Letter Vocalic RR
+    "&#x0961", //Letter Vocalic LL
+
+    //Independent vowel for Marathi
+    "&3x0972" //Letter Candra A
+];
+
+var consonants = [
+    "&#x0915", //Letter KA
+    "&#x0916", //Letter KHA
+    "&#x0917", //Letter GA
+    "&#x0918", //Letter GHA
+    "&#x0919", //Letter NGA
+    "&#x091A", //Letter CA
+    "&#x091B", //Letter CHA
+    "&#x091C", //Letter JA
+    "&#x091D", //Letter JHA
+    "&#x091E", //Letter NYA
+    "&#x091F", //Letter TTA
+    "&#x0920", //Letter TTHA
+    "&#x0921", //Letter DDA
+    "&#x0922", //Letter DDHA
+    "&#x0923", //Letter NNA
+    "&#x0924", //Letter TA
+    "&#x0925", //Letter THA
+    "&#x0926", //Letter DA
+    "&#x0927", //Letter DHA
+    "&#x0928", //Letter NA
+    "&#x0929", //Letter NNNA, NA with nukta
+    "&#x092A", //Letter PA
+    "&#x092B", //Letter PHA
+    "&#x092C", //Letter BA
+    "&#x092D", //Letter BHA
+    "&#x092E", //Letter MA
+    "&#x092F", //Letter YA
+    "&#x0930", //Letter RA
+    "&#x0931", //Letter RRA, RA with nukta
+    "&#x0932", //Letter LA
+    "&#x0933", //Letter LLA
+    "&#x0934", //Letter LLLA, LLA with nukta
+    "&#x0935", //Letter VA
+    "&#x0936", //Letter SHA
+    "&#x0937", //Letter SSA
+    "&#x0938", //Letter SA
+    "&#x0939", //Letter HA
+
+    //Additional consonants
+	"&#x0958", //Letter QA, KA with nukta
+    "&#x0959", //Letter KHHA, KHA with nukta
+    "&#x095A", //Letter GA, LLA with nukta
+    "&#x095B", //Letter ZA, JA with nukta
+    "&#x095C", //Letter DDDHA, DDA with nukta
+    "&#x095D", //Letter RHA, DDHA with nukta
+    "&#x095E", //Letter FA, PHA with nukta
+    "&#x095F" //Letter YYA, YA with nukta
+];
+
+var dependent_vowels = [
+    "&#x093E", //Vowel Sign AA
+    "&#x093F", //Vowel Sign I
+    "&#x0940", //Vowel Sign II
+    "&#x0941", //Vowel Sign U
+    "&#x0942", //Vowel Sign UU
+    "&#x0943", //Vowel Sign Vocalic R
+    "&#x0944", //Vowel Sign Vocalic RR
+    "&#x0945", //Letter Candra E
+    "&#x0946", //Letter Short E
+    "&#x0947", //Letter E 
+    "&#x0948", //Letter AI
+    "&#x0949", //Letter Candra O
+    "&#x094A", //Letter Short O
+    "&#x094B", //Letter O
+    "&#x094C", //Letter AU
+
+    //Additional vowel signs for Sanskrit
+    "&#x0962", //Vowel Vocalic L
+    "&#x0963" //Vowel Vocalic LL
+];
+
+var various_signs = [
+    "&#x0901", //Sign Candrabindu
+    "&#x0902", //Sign Anusvara
+    "&#x0903", //Sign Visarga
+    "&#x094D" //Sign Virama
+];
+
+var virama = "&#x094D"; //Sign Virama (Halant)
