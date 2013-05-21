fd8800b282bc4eecb27c8dfeb5f285ce751d1318
Prateek Saxena <prtksxna@gmail.com>
2013-05-21 14:24:41 +0530

    Fixing the format of the Unicode definitions

--- res/character-set.js
+++ res/character-set.js
@@ -1,107 +1,107 @@
 var independent_vowels = [
-    "&#x0904", //Letter Short A
-    "&#x0905", //Letter A
-    "&#x0906", //Letter AA
-    "&#x0907", //Letter I
-    "&#x0908", //Letter II
-    "&#x0909", //Letter U
-    "&#x090A", //Letter UU
-    "&#x090B", //Letter Vocalic R
-    "&#x090C", //Letter Vocalic L
-    "&#x090D", //Letter Candra E 
-    "&#x090E", //Letter Short E
-    "&#x090F", //Letter E
-    "&#x0910", //Letter AI
-    "&#x0911", //Letter Candra O
-    "&#x0912", //Letter Short O
-    "&#x0913", //Letter O
-    "&#x0914", //Letter AU
+    "\u0904", //Letter Short A
+    "\u0905", //Letter A
+    "\u0906", //Letter AA
+    "\u0907", //Letter I
+    "\u0908", //Letter II
+    "\u0909", //Letter U
+    "\u090A", //Letter UU
+    "\u090B", //Letter Vocalic R
+    "\u090C", //Letter Vocalic L
+    "\u090D", //Letter Candra E
+    "\u090E", //Letter Short E
+    "\u090F", //Letter E
+    "\u0910", //Letter AI
+    "\u0911", //Letter Candra O
+    "\u0912", //Letter Short O
+    "\u0913", //Letter O
+    "\u0914", //Letter AU
 
     //Additional vowels for Sanskrit
-    "&#x0960", //Letter Vocalic RR
-    "&#x0961", //Letter Vocalic LL
+    "\u0960", //Letter Vocalic RR
+    "\u0961", //Letter Vocalic LL
 
     //Independent vowel for Marathi
     "&3x0972" //Letter Candra A
 ];
 
 var consonants = [
-    "&#x0915", //Letter KA
-    "&#x0916", //Letter KHA
-    "&#x0917", //Letter GA
-    "&#x0918", //Letter GHA
-    "&#x0919", //Letter NGA
-    "&#x091A", //Letter CA
-    "&#x091B", //Letter CHA
-    "&#x091C", //Letter JA
-    "&#x091D", //Letter JHA
-    "&#x091E", //Letter NYA
-    "&#x091F", //Letter TTA
-    "&#x0920", //Letter TTHA
-    "&#x0921", //Letter DDA
-    "&#x0922", //Letter DDHA
-    "&#x0923", //Letter NNA
-    "&#x0924", //Letter TA
-    "&#x0925", //Letter THA
-    "&#x0926", //Letter DA
-    "&#x0927", //Letter DHA
-    "&#x0928", //Letter NA
-    "&#x0929", //Letter NNNA, NA with nukta
-    "&#x092A", //Letter PA
-    "&#x092B", //Letter PHA
-    "&#x092C", //Letter BA
-    "&#x092D", //Letter BHA
-    "&#x092E", //Letter MA
-    "&#x092F", //Letter YA
-    "&#x0930", //Letter RA
-    "&#x0931", //Letter RRA, RA with nukta
-    "&#x0932", //Letter LA
-    "&#x0933", //Letter LLA
-    "&#x0934", //Letter LLLA, LLA with nukta
-    "&#x0935", //Letter VA
-    "&#x0936", //Letter SHA
-    "&#x0937", //Letter SSA
-    "&#x0938", //Letter SA
-    "&#x0939", //Letter HA
+    "\u0915", //Letter KA
+    "\u0916", //Letter KHA
+    "\u0917", //Letter GA
+    "\u0918", //Letter GHA
+    "\u0919", //Letter NGA
+    "\u091A", //Letter CA
+    "\u091B", //Letter CHA
+    "\u091C", //Letter JA
+    "\u091D", //Letter JHA
+    "\u091E", //Letter NYA
+    "\u091F", //Letter TTA
+    "\u0920", //Letter TTHA
+    "\u0921", //Letter DDA
+    "\u0922", //Letter DDHA
+    "\u0923", //Letter NNA
+    "\u0924", //Letter TA
+    "\u0925", //Letter THA
+    "\u0926", //Letter DA
+    "\u0927", //Letter DHA
+    "\u0928", //Letter NA
+    "\u0929", //Letter NNNA, NA with nukta
+    "\u092A", //Letter PA
+    "\u092B", //Letter PHA
+    "\u092C", //Letter BA
+    "\u092D", //Letter BHA
+    "\u092E", //Letter MA
+    "\u092F", //Letter YA
+    "\u0930", //Letter RA
+    "\u0931", //Letter RRA, RA with nukta
+    "\u0932", //Letter LA
+    "\u0933", //Letter LLA
+    "\u0934", //Letter LLLA, LLA with nukta
+    "\u0935", //Letter VA
+    "\u0936", //Letter SHA
+    "\u0937", //Letter SSA
+    "\u0938", //Letter SA
+    "\u0939", //Letter HA
 
     //Additional consonants
-	"&#x0958", //Letter QA, KA with nukta
-    "&#x0959", //Letter KHHA, KHA with nukta
-    "&#x095A", //Letter GA, LLA with nukta
-    "&#x095B", //Letter ZA, JA with nukta
-    "&#x095C", //Letter DDDHA, DDA with nukta
-    "&#x095D", //Letter RHA, DDHA with nukta
-    "&#x095E", //Letter FA, PHA with nukta
-    "&#x095F" //Letter YYA, YA with nukta
+        "\u0958", //Letter QA, KA with nukta
+    "\u0959", //Letter KHHA, KHA with nukta
+    "\u095A", //Letter GA, LLA with nukta
+    "\u095B", //Letter ZA, JA with nukta
+    "\u095C", //Letter DDDHA, DDA with nukta
+    "\u095D", //Letter RHA, DDHA with nukta
+    "\u095E", //Letter FA, PHA with nukta
+    "\u095F" //Letter YYA, YA with nukta
 ];
 
 var dependent_vowels = [
-    "&#x093E", //Vowel Sign AA
-    "&#x093F", //Vowel Sign I
-    "&#x0940", //Vowel Sign II
-    "&#x0941", //Vowel Sign U
-    "&#x0942", //Vowel Sign UU
-    "&#x0943", //Vowel Sign Vocalic R
-    "&#x0944", //Vowel Sign Vocalic RR
-    "&#x0945", //Letter Candra E
-    "&#x0946", //Letter Short E
-    "&#x0947", //Letter E 
-    "&#x0948", //Letter AI
-    "&#x0949", //Letter Candra O
-    "&#x094A", //Letter Short O
-    "&#x094B", //Letter O
-    "&#x094C", //Letter AU
+    "\u093E", //Vowel Sign AA
+    "\u093F", //Vowel Sign I
+    "\u0940", //Vowel Sign II
+    "\u0941", //Vowel Sign U
+    "\u0942", //Vowel Sign UU
+    "\u0943", //Vowel Sign Vocalic R
+    "\u0944", //Vowel Sign Vocalic RR
+    "\u0945", //Letter Candra E
+    "\u0946", //Letter Short E
+    "\u0947", //Letter E
+    "\u0948", //Letter AI
+    "\u0949", //Letter Candra O
+    "\u094A", //Letter Short O
+    "\u094B", //Letter O
+    "\u094C", //Letter AU
 
     //Additional vowel signs for Sanskrit
-    "&#x0962", //Vowel Vocalic L
-    "&#x0963" //Vowel Vocalic LL
+    "\u0962", //Vowel Vocalic L
+    "\u0963" //Vowel Vocalic LL
 ];
 
 var various_signs = [
-    "&#x0901", //Sign Candrabindu
-    "&#x0902", //Sign Anusvara
-    "&#x0903", //Sign Visarga
-    "&#x094D" //Sign Virama
+    "\u0901", //Sign Candrabindu
+    "\u0902", //Sign Anusvara
+    "\u0903", //Sign Visarga
+    "\u094D" //Sign Virama
 ];
 
-var virama = "&#x094D"; //Sign Virama (Halant)
+var virama = "\u094D"; //Sign Virama (Halant)
